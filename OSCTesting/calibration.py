4260735fbc300e481c21b7df67b4dd07d9a04d56
eggsonafryingpan <eggsonafryingpan@gmail.com>
2026-07-15 00:45:37 -0400

    fixed mag for calibration

--- OSCTesting/calibration.py
+++ OSCTesting/calibration.py
@@ -7,8 +7,11 @@ import numpy as np
 
 last_process = time.monotonic()
 
+# 0 = GYRO ACC   1 = MAG
+calibration_stage = 0
+
 #in seconds
-CALIBRATION_PERIOD = 5
+CALIBRATION_PERIOD = 10
 
 sensors = {
     "GYRO": {
@@ -33,14 +36,22 @@ calibration = [[0,0,0],[0,0,-9.81],[0,0,0]]
 def calc_calibration():
     #GYRO calibration is on in deg
     for index, axis_data in enumerate(sensors["GYRO"].values()):
+        if len(axis_data) == 0:
+            raise ValueError("GYRO data cannot be empty, check OSC")
         data = np.array(axis_data)
         mean = np.mean(data)
         calibration[0][index] = mean
     for index, axis_data in enumerate(sensors["ACC"].values()):
+        if len(axis_data) == 0:
+            raise ValueError("ACC data cannot be empty, check OSC")
         data = np.array(axis_data)
         mean = np.mean(data)
         calibration[1][index] += mean
+
+def calc_calibration_MAG():
     for index, axis_data in enumerate(sensors["MAG"].values()):
+        if len(axis_data) == 0:
+            raise ValueError("MAG data cannot be empty, check OSC")
         data = np.array(axis_data)
         data_min = data.min(axis=0)
         data_max = data.max(axis=0)
@@ -56,14 +67,27 @@ def write_calibration():
 
 
 def timer():
-    global last_process
+    global last_process, calibration_stage
+    print("Calibrating GYRO and ACC...")
     while True:
         if (time.monotonic() - last_process >= CALIBRATION_PERIOD):
             calc_calibration()
-            write_calibration()
-            print("Calibration finished :D")
-            sys.exit(0)
+            print("Calibration for GYRO and ACC finished :D")
+            break
         time.sleep(0.01)
+    time.sleep(0.5)
+    print("Prepare calibration for MAG")
+    time.sleep(3)
+    print("Calibrating MAG...")
+    calibration_stage = 1
+    last_process = time.monotonic()
+    while True:
+        if (time.monotonic() - last_process >= CALIBRATION_PERIOD):
+            calc_calibration_MAG()
+            print("Calibration for MAG finished :D")
+            break
+        time.sleep(0.01)
+    write_calibration()
 
 threading.Thread(target=timer,daemon=True).start()
 
@@ -80,6 +104,10 @@ def handler(address, *args):
     sensor_name = sensor_info[0]
     axis = sensor_info[1]
 
+    if sensor_name == "MAG" and calibration_stage != 1:
+        return
+    if (sensor_name == "GYRO" or sensor_name == "ACC") and calibration_stage !=0:
+        return
     sensors[sensor_name][axis].append(val)
 
 
@@ -90,5 +118,4 @@ dispatcher.set_default_handler(handler)
 
 
 server = BlockingOSCUDPServer(("127.0.0.1", 12345), dispatcher)
-print("Recieving...")
 server.serve_forever()
